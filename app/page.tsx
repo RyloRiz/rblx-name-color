9db90479d51c519d7c48f613198f57bac63b3582
RyloRiz <91710711+RyloRiz@users.noreply.github.com>
2023-11-16 01:52:59 -0800

    v1.0.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,66 @@
+'use client';
+
 import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect, useRef, useState } from 'react'
+import GetNameColor from '@/scripts/generator'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const [username, setUsername] = useState<string>('R0bl0x10501050');
+	const [version, setVersion] = useState<number>(3);
+	const colorRef = useRef<HTMLDivElement>(null);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	useEffect(() => {
+		// if (username.length < 2) {
+		// 	colorRef.current!.style.backgroundColor = 'rgb(' + [0, 0, 0, 0].join(',') + ')';
+		// 	colorRef.current!.innerText = 'Username too short';
+		// 	colorRef.current!.style.color = 'rgb(255, 0, 0, 1)';
+		//	return;
+		// };
+		let color = GetNameColor(username, version);
+		if (color) {
+			colorRef.current!.style.color = 'rgb(255, 0, 0, 0)';
+			colorRef.current!.style.backgroundColor = 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
+			colorRef.current!.innerText = '.';
+		} else {
+			colorRef.current!.style.backgroundColor = 'rgb(' + [0, 0, 0, 0].join(',') + ')';
+			colorRef.current!.innerText = 'Impossible to display color';
+			colorRef.current!.style.color = 'rgb(255, 0, 0, 1)';
+		}
+	}, [username]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (<>
+		<div className="w-screen h-screen flex flex-col justify-center items-center">
+			<div className="flex flex-grow"></div>
+			<div className="font-bold text-5xl text-white">Roblox Name Color</div>
+			<div className="mt-5 text-lg text-slate-300">Find a user&apos;s chat color. Inspired by&nbsp;
+				<Link href='https://devforum.roblox.com/t/your-name-color-in-chat-%E2%80%94-history-and-how-it-works/2702247' className='text-blue-500 transition duration-200 hover:text-blue-400'>
+					this DevForum post
+				</Link>
+				.
+			</div>
+			<div className="mt-5 flex flex-row justify-center space-x-6">
+				<input onInput={() => setUsername((document.getElementById('username') as HTMLInputElement).value)} autoComplete='off' type="text" name="username" id="username" className='px-5 py-3 bg-slate-900 border-slate-700 border-2 text-white rounded-lg placeholder:text-slate-300 focus:border-blue-600 focus:outline-none transition duration-200' value={username} />
+				<div className="flex flex-row justify-center space-x-3">
+					<button onClick={() => setVersion(1)} className={(version === 1 ? 'bg-blue-500 text-slate-200 hover:text-white' : 'bg-slate-800 text-blue-500 hover:text-blue-400') + ` px-5 py-3 text-center rounded-lg font-bold transition duration-200`}>v1</button>
+					<button onClick={() => setVersion(2)} className={(version === 2 ? 'bg-blue-500 text-slate-200 hover:text-white' : 'bg-slate-800 text-blue-500 hover:text-blue-400' )+ ` px-5 py-3 text-center rounded-lg font-bold transition duration-200`}>v2</button>
+					<button onClick={() => setVersion(3)} className={(version === 3 ? 'bg-blue-500 text-slate-200 hover:text-white' : 'bg-slate-800 text-blue-500 hover:text-blue-400') + ` px-5 py-3 text-center rounded-lg font-bold transition duration-200`}>v3</button>
+				</div>
+			</div>
+			<div ref={colorRef} className="mt-5 px-10 py-3 text-center rounded-lg transition duration-200">.</div>
+			<div className="flex flex-grow"></div>
+			{/* <hr className='border-slate-300' /> */}
+			<div className="my-5 text-slate-300">
+				<Link href='https://github.com/RyloRiz' className='text-blue-500 transition duration-200 hover:text-blue-400'>
+					@RyloRiz
+				</Link>
+				&nbsp;on GitHub |&nbsp;
+				<Link href='https://devforum.roblox.com/u/r0bl0x10501050' className='text-blue-500 transition duration-200 hover:text-blue-400'>
+					@R0bl0x10501050
+				</Link>
+				&nbsp;on Roblox &amp; DevForum
+			</div>
+		</div>
+	</>)
 }
---
+++ scripts/generator.ts
@@ -0,0 +1,91 @@
+class Color {
+	public r: number;
+	public g: number;
+	public b: number;
+	constructor(r: number, g: number, b: number) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+	}
+}
+
+let CHAT_COLORS_BY_VERSION_FN = () => {
+	let fakePersimmon = new Color(253, 41, 67);
+	let fakeCyan = new Color(1, 162, 255);
+	let fakeDarkGreen = new Color(2, 184, 87);
+	let brightViolet = new Color(107, 50, 124)
+
+	let t = [
+		[
+			new Color(196, 40, 28), // Bright red
+			new Color(13, 105, 172), // Bright blue
+			new Color(39, 70, 45), // Earth green
+			brightViolet
+		],
+		[
+			fakePersimmon,
+			fakeCyan,
+			fakeDarkGreen,
+			brightViolet
+		],
+		[
+			fakePersimmon,
+			fakeCyan,
+			fakeDarkGreen,
+			new Color(180, 128, 255) // Alder
+		]
+	]
+
+	let unchangedColors = [
+		new Color(218, 133, 65), // Bright orange
+		new Color(245, 205, 48), // Bright yellow
+		new Color(232, 186, 200), // Light reddish violet
+		new Color(215, 197, 154), // Brick yellow
+	]
+
+	function move(src: any[], a: number, b: number, t: number, dist: any[]) {
+		// dist[t], ... = src[a], ..., src[b]
+		for (let i = 0; i <= b - a; i++) {
+			let srcElement = src[i + a];
+			let distIdx = t + i;
+			// console.log(a, i, b, t, srcElement, distIdx);
+			dist[distIdx] = srcElement;
+		}
+		return dist;
+	}
+
+	for (let i = 0; i < t.length; i++) {
+		let colors = t[i];
+		t[i] = move(unchangedColors, 0, unchangedColors.length - 1, colors.length, colors);
+	}
+
+	Object.freeze(t);
+	return t;
+}
+
+let CHAT_COLORS_BY_VERSION = CHAT_COLORS_BY_VERSION_FN();
+
+let ComputeNameValue = (username: string) => {
+	let value = 0;
+	for (let index = 0; index <= username.length - 1; index++) {
+		let cVal = username.substring(index, index + 1)
+		let cValue = cVal.charCodeAt(0);
+		let reverseIndex = username.length - index;
+		if (username.length % 2 === 1) {
+			reverseIndex -= 1;
+		}
+		if (reverseIndex % 4 >= 2) {
+			cValue = -cValue;
+		}
+		value += cValue;
+	}
+	return value;
+}
+
+let GetNameColor = (username: string, version_?: number) => {
+	let chatColors = CHAT_COLORS_BY_VERSION[typeof version_ === 'number' ? version_ - 1 : CHAT_COLORS_BY_VERSION.length - 1];
+	let value = (ComputeNameValue(username) % chatColors.length) + 1;
+	return chatColors[value];
+}
+
+export default GetNameColor;
